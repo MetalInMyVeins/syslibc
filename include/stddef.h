e503c999d38c65bde60566123db19db1f9086ab9
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-09-21 13:24:00 +0600

    stddef: add architecture check and define size_t

---
+++ include/stddef.h
@@ -0,0 +1,12 @@
+#ifndef STDDEF_H
+#define STDDEF_H
+
+#if !defined(__x86_64__) || !defined(__linux__)
+#error "syslibc: Supported platform: x86_64 Linux (SYSV)"
+#endif
+
+static_assert(sizeof(unsigned long) == 8, "syslibc: LP64 target required");
+
+typedef unsigned long size_t;
+
+#endif // STDDEF_H
